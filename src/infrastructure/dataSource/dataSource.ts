8d901f70d0f2b50b644cc49d1ff261d2ae1e13fd
japacx <jepacheco98@gmail.com>
2024-01-21 22:45:21 -0500

    fix hexagonal implementation

--- src/infrastructure/dataSource/dataSource.ts
+++ src/infrastructure/dataSource/dataSource.ts
@@ -2,8 +2,8 @@ import { GithubRepositoryInterface } from "../../domain/interfaces/repository";
 import { GithubRepositoryEntity } from "../../domain/entities/repository";
 
 const newEntity = new GithubRepositoryEntity();
-newEntity.id = 1;
-newEntity.name = "github-repository-1";
+newEntity.title = 1;
+newEntity.author = "github-repository-1";
 
 export class DataSourceRepository implements GithubRepositoryInterface {
   async getAll(): Promise<GithubRepositoryEntity[] | Error> {
